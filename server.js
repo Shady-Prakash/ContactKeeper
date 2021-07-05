bcfc13184b9aca14522fc644931f5b0e9d24cb50
Prakash Mahat <shadyprakash8@gmail.com>
2021-07-05 16:48:26 +0545

    user model and validation

--- server.js
+++ server.js
@@ -8,6 +8,9 @@ const app = express();
 // Connect a Database
 connectDB();
 
+// Init Middleware
+app.use(express.json({extended: false}));
+
 app.get('/', (req, res) =>
 	res.json({msg: 'Welcome to the ContactKeeper API...'})
 );
