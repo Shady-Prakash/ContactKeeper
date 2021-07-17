dcaa8aad5ef340fa52c23cbfc0d1faa174329562
Prakash Mahat <shadyprakash8@gmail.com>
2021-07-17 17:01:40 +0545

    load user and set token

--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -1,11 +1,20 @@
 /** @format */
 
-import React from 'react';
+import React, {useContext, useEffect} from 'react';
 import Contacts from '../contacts/Contacts';
 import ContactForm from '../contacts/ContactForm';
 import ContactFilter from '../../context/contact/ContactFilter';
+import AuthContext from '../../context/auth/authContext';
 
 const Home = () => {
+	const authContext = useContext(AuthContext);
+
+	useEffect(() => {
+		authContext.loadUser();
+
+		// eslint-disable-next-line
+	}, []);
+
 	return (
 		<div className='grid-2'>
 			<div>
