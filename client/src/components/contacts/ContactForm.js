a40f99aa2a808866c6bc1a89d5ce79b272f16bda
Prakash Mahat <shadyprakash8@gmail.com>
2021-07-18 21:21:16 +0545

    Update Contacts

--- client/src/components/contacts/ContactForm.js
+++ client/src/components/contacts/ContactForm.js
@@ -40,12 +40,7 @@ const ContactForm = () => {
 		} else {
 			updateContact(contact);
 		}
-		setContact({
-			name: '',
-			email: '',
-			phone: '',
-			type: 'personal',
-		});
+		clearAll();
 	};
 
 	const clearAll = () => {
