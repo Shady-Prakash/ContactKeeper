0031e10906da2d9a0a48499a77bb4d71c0d07eb5
Prakash Mahat <shadyprakash8@gmail.com>
2021-07-18 20:57:45 +0545

    delete contacts

---
+++ client/src/components/layout/Spinner.js
@@ -0,0 +1,17 @@
+/** @format */
+
+import React, {Fragment} from 'react';
+import spinner from './spinner.gif';
+const Spinner = () => {
+	return (
+		<Fragment>
+			<img
+				src={spinner}
+				style={{width: '200px', margin: 'auto', display: 'block'}}
+				alt='Loading...'
+			/>
+		</Fragment>
+	);
+};
+
+export default Spinner;
